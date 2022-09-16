c27e0cdaaf372d15dab06309e359d9ae009da429
ilnarkz <ilnar-16_04@mail.ru>
2022-09-16 22:53:25 +0300

    fix issue

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -50,10 +50,12 @@ class UserDeleteView(UserPassesTestMixin, SuccessMessageMixin, DeleteView):
         return redirect(self.error_url)
 
     def form_valid(self, form):
+        success_message = self.success_message
         if Task.objects.filter(author=self.request.user):
             messages.error(self.request, _("It is not possible to delete a user because it is in use"))
             return redirect(self.error_url)
         self.object.delete()
+        messages.success(self.request, success_message)
         return redirect(reverse('users:users'))
 
 
